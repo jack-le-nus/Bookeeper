7b522d2684eda4c53b5845bd0f3085385b704c44
Jack Le <jackle@Jacks-MBP.mshome.net>
2017-04-11 16:18:44 +0800

    Update Loading View

--- MTECHProgramme/LoadingView.swift
+++ MTECHProgramme/LoadingView.swift
@@ -22,16 +22,17 @@ public class LoadingView{
     }
     
     public func show(view: UIView) {
-    
+        
         overlayView.frame = CGRect(x: 0, y: 0, width: 80, height: 80)
         overlayView.center = view.center
-        overlayView.backgroundColor = UIColor(white: 0x444444, alpha: 0.7)
+        overlayView.backgroundColor = UIColor.clear
         overlayView.clipsToBounds = true
         overlayView.layer.cornerRadius = 10
         
         activityIndicator.frame = CGRect(x: 0, y: 0, width: 40, height: 40)
         activityIndicator.activityIndicatorViewStyle = .whiteLarge
         activityIndicator.center = CGPoint(x: overlayView.bounds.width / 2, y: overlayView.bounds.height / 2)
+        activityIndicator.color = UIColor.gray
         
         overlayView.addSubview(activityIndicator)
         view.addSubview(overlayView)
