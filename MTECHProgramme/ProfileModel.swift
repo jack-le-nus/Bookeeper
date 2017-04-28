a6b2ba3f72a67e0ec963d1aea62f4ec90af61d9e
Ritesh Kumar <riteshkumar@riteshs-mac.local>
2017-04-28 17:58:13 +0800

    added profile model

---
+++ MTECHProgramme/ProfileModel.swift
@@ -0,0 +1,56 @@
+//
+//  ProfileModel.swift
+//  MTECHProgramme
+//
+//  Created by Medha Sharma on 4/20/17.
+//
+//
+
+import Foundation
+import Firebase
+
+class ProfileModel
+{
+    var ref: FIRDatabaseReference!
+    //to validate user phone number
+    func isValidNumber(number: String) -> Bool {
+        do {
+            let detector = try NSDataDetector(types: NSTextCheckingResult.CheckingType.phoneNumber.rawValue)
+            let matches = detector.matches(in: number, options: [], range: NSMakeRange(0, number.characters.count))
+            if let res = matches.first {
+                return res.resultType == .phoneNumber && res.range.location == 0 && res.range.length == number.characters.count
+            }
+            else {
+                return false
+            }
+        }
+        catch {
+            return false
+        }
+    }
+    
+    //to validate user email id
+    func isValidEmail(eMail: String) -> Bool {
+        do {
+            let regex = try NSRegularExpression(pattern: "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}", options: .caseInsensitive)
+            return regex.firstMatch(in: eMail, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, eMail.characters.count)) != nil
+        }
+        catch {
+            return false
+        }
+    }
+    
+    //to validate user name
+    func isValidName(name: String) -> Bool
+    {
+        let letters = NSCharacterSet.letters
+        let range = name.rangeOfCharacter(from: letters, options: .caseInsensitive)
+        // range will be nil if no letters is found
+        if range != nil {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+}
